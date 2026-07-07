458516cd8f551f89d23de9b2ca8a6b9e22133c88
Lee Andrew <garden4kent@gmail.com>
2026-07-07 23:03:33 +0100

    Add GA4 tracking (G-0DHBJ4FEQX)

--- app/layout.js
+++ app/layout.js
@@ -76,6 +76,23 @@ export default function RootLayout({ children }) {
             alt=""
           />
         </noscript>
+        <Script
+          id="ga4"
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0DHBJ4FEQX"
+        />
+        <Script
+          id="ga4-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0DHBJ4FEQX');
+            `,
+          }}
+        />
       </body>
     </html>
   );
