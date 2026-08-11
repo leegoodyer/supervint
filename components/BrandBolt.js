eefe0ec7475b17a63f78084953a2f4e8bcb54f68
Lee Andrew <garden4kent@gmail.com>
2026-08-11 17:03:20 +0100

    fix: BrandBolt uses yellow #FFD400 bolt to match site logo (was white)

--- components/BrandBolt.js
+++ components/BrandBolt.js
@@ -2,7 +2,7 @@ export default function BrandBolt({ sm = false }) {
   return (
     <span className={`brand-bolt${sm ? ' sm' : ''}`}>
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" fill="#fff" />
+        <path d="M13 2L3 14h9l-1 8 10-12h-9l1-8z" fill="#FFD400" />
       </svg>
     </span>
   );
